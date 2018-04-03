8e49c17e4625149a35f19eaf3c4f7e8ba9b2de02
Helden,Laura L.H.A. van <l.vanhelden@student.fontys.nl>
2018-04-03 16:57:11 +0200

    add all the unity

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GemType
+{
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Purple,
+    Orange,
+    White
+}
+public class Gem : MonoBehaviour {
+    // Update is called once per frame
+    public GemType type;
+    public int score;
+    public int damage;
+	void Update ()
+    {
+		
+	}
+}
